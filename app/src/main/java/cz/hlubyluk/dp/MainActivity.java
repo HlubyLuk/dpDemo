2333fa5f037aa1939c34cc39a36bf4c16dceb091
HlubyLuk <lukas.hlubucek@gmail.com>
2015-02-28 13:11:17 +0100

    Demo ukázka spuštění activity a předání textu

--- app/src/main/java/cz/hlubyluk/dp/MainActivity.java
+++ app/src/main/java/cz/hlubyluk/dp/MainActivity.java
@@ -1,39 +1,63 @@
 package cz.hlubyluk.dp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.mdOpen, R.string.mdClose);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        LinearLayout navigation = (LinearLayout) findViewById(R.id.mdRL);
+        for (int i = 0; i < navigation.getChildCount(); i++) {
+            if (navigation.getChildAt(i) instanceof Button) {
+                navigation.getChildAt(i).setOnClickListener(this);
+            }
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.wtf("ma-onClick", ((Button) view).getText().toString());
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.mdBT1:
+                intent = new Intent(this, OtherUI.class);
+                intent.putExtra("text", ((Button) view).getText());
+                break;
+            case R.id.mdBT2:
+                intent = new Intent(this, OtherUI.class);
+                intent.putExtra("text", ((Button) view).getText());
+                break;
+        }
+        startActivity(intent);
+        drawerLayout.closeDrawers();
     }
 }
